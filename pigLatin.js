6b644712ccc43049fb5799c94b4bce13b0cc0b71
charlesstocky <charles.stocky@gmail.com>
2018-01-29 02:08:57 -0600

    igpay atlinay eneratorgay

---
+++ pigLatin.js
@@ -0,0 +1,21 @@
+/*
+
+Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+Examples
+
+pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+pigIt('Hello world !');     // elloHay orldWay !
+
+*/
+
+function pigIt(str){
+  let splitStr = str.split(' ');  
+  let pigStr = ''
+  for(var i = 0; i < splitStr.length; i++){
+    let firstLetter = splitStr[i].slice(0, 1)
+    splitStr[i] = splitStr[i].slice(1) + firstLetter + 'ay'
+  }
+  console.log(splitStr.join(' '))
+}
+
+pigIt('Pig latin is cool')
